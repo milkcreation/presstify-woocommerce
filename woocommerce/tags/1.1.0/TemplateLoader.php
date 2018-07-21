d33635c133b96d42cd21495ff8f21447e2095f96
mastermilk <mastermilk@9c4951e9-fce0-42f0-91e9-f5562070ff10>
2018-07-21 13:14:42 +0000

    woocommerce -- tag1.1.0 -- Compatibilité tiFy 1.4.53 + surcharge templates Woocommerce

---
+++ woocommerce/tags/1.1.0/TemplateLoader.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace tiFy\Plugins\WooCommerce;
+
+use tiFy\App\AbstractAppController;
+
+class TemplateLoader extends AbstractAppController
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function boot()
+    {
+        $this->app->appAddFilter('template_include', [$this, 'template_include'], 99);
+        $this->app->appAddFilter('wc_get_template', [$this, 'wc_get_template'], 10, 5);
+        $this->app->appAddFilter('wc_get_template_part', [$this, 'wc_get_template_part'], 10, 3);
+    }
+
+    /**
+     * Chargement d'un template WooCommerce.
+     *
+     * @param string $template Chemin absolu du fichier de template à charger.
+     * @param array $args Attributs du template.
+     *
+     * @return void
+     */
+    public function loadWooTemplate($template = '', $args = [])
+    {
+        $folder = 'wc';
+
+        if (preg_match('#' . preg_quote(get_stylesheet_directory(), DIRECTORY_SEPARATOR) . '#', $template)) :
+            $directory = get_stylesheet_directory() . DIRECTORY_SEPARATOR . WC()->template_path();
+        else :
+            $directory = WC()->plugin_path() . '/templates' . DIRECTORY_SEPARATOR;
+        endif;
+
+        $this->app->appTemplates()->removeFolder($folder);
+        $this->app->appTemplates()->addFolder($folder, $directory, true);
+
+        $patterns = $replacements = [];
+        $patterns[] = '#' . preg_quote($directory, DIRECTORY_SEPARATOR) . '#';
+        $patterns[] = '#\.php?$#';
+        $replacements[] = '';
+        $path = preg_replace($patterns, $replacements, $template);
+        $name = "{$folder}::{$path}";
+        echo $this->app->appTemplateRender($name, $args);
+    }
+
+    /**
+     * Court-cicuitage du chemin vers le gabarit d'affichage.
+     *
+     * @param string $template Chemin vers le gabarit d'affichage.
+     *
+     * @return string
+     */
+    public function template_include($template)
+    {
+        if (is_woocommerce()) :
+            if (preg_match('#' . preg_quote(get_stylesheet_directory(), DIRECTORY_SEPARATOR) . '#', $template)) :
+                $directory = get_stylesheet_directory() . DIRECTORY_SEPARATOR . WC()->template_path();
+            else :
+                $directory = WC()->plugin_path() . '/templates' . DIRECTORY_SEPARATOR;
+            endif;
+
+            $template = preg_replace(
+                '#' . preg_quote($directory, DIRECTORY_SEPARATOR) . '#',
+                '',
+                $template
+            );
+            $this->app->appTemplates()->addFolder('wc', $directory, true);
+
+            $template = "wc::{$template}";
+        endif;
+
+        return $template;
+    }
+
+    /**
+     * Surcharge du moteur de template WooCommerce.
+     *
+     * @param string $located Chemin absolu du fichier de template à charger.
+     * @param string $template_name Nom du template.
+     * @param array $args Attributs du template.
+     * @param string $template_path
+     * @param string $default_path
+     *
+     * @return string
+     */
+    public function wc_get_template($located, $template_name, $args, $template_path, $default_path)
+    {
+        if (is_admin() && !wp_doing_ajax()) :
+            return $located;
+        endif;
+
+        $this->loadWooTemplate($located, $args);
+
+        return dirname(__FILE__) . '/index.php';
+    }
+
+    /**
+     * Surcharge du moteur de template (partiel) WooCommerce.
+     *
+     * @param string $template Chemin absolu du fichier de template partiel à charger.
+     * @param string $slug Identifiant du fichier de template partiel.
+     * @param string $name Nom du fichier de template partiel.
+     *
+     * @return bool
+     */
+    public function wc_get_template_part($template, $slug, $name)
+    {
+        $this->loadWooTemplate($template);
+
+        return $template = false;
+    }
+}
