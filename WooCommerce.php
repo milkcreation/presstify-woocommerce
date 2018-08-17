2c594a6ea428edd771ed6886a3129cf6c8124004
Jordy Manner <jordy.manner@milkcreation.fr>
2018-08-17 09:23:40 +0000

    branch 1.0 -- Correction du numéro de version à la branche correspondante

--- WooCommerce.php
+++ WooCommerce.php
@@ -3,7 +3,7 @@
 Plugin Name: Woocommerce
 Plugin URI: http://presstify.com/plugins/woocommerce
 Description: Support de Woocommerce
-Version: 1.1.5
+Version: 1.0.3
 Author: Milkcreation
 Author URI: http://milkcreation.fr
 */
