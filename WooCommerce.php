030d757d9a232aca7d809ffca31c578e2de93f48
Jordy Manner <jordy.manner@milkcreation.fr>
2018-08-20 16:55:38 +0000

    woocommerce -- trunk -- Mise à jour du n° de version + évolution surchage + helpers phase #1

--- WooCommerce.php
+++ WooCommerce.php
@@ -1,111 +1,130 @@
 <?php
-/*
-Plugin Name: Woocommerce
-Plugin URI: http://presstify.com/plugins/woocommerce
-Description: Support de Woocommerce
-Version: 1.1.5
-Author: Milkcreation
-Author URI: http://milkcreation.fr
-*/
 
 /**
- * PLUGIN DE SUPPORT WOOCOMMERCE
+ * @name WooCommerce
+ * @desc Support de Woocommerce
+ * @author Jordy Manner <jordy@milkcreation.fr> && Julien Picard <julien@tigreblanc.fr>
+ * @package presstify-plugins/woocommerce
+ * @namespace \tiFy\Plugins\WooCommerce
+ * @version 1.4.1
+ */
+
+/**
  * @see https://docs.woocommerce.com/wc-apidocs/index.html
  */
+
 namespace tiFy\Plugins\WooCommerce;
 
 use tiFy\App\Plugin;
+use tiFy\Plugins\WooCommerce\Admin\Product\MetaBoxes\MetaBoxes;
+use tiFy\Plugins\WooCommerce\Cart;
+use tiFy\Plugins\WooCommerce\CheckOut;
+use tiFy\Plugins\WooCommerce\ConditionalTags;
+use tiFy\Plugins\WooCommerce\Emails;
+use tiFy\Plugins\WooCommerce\Forms;
+use tiFy\Plugins\WooCommerce\MultiShop\MultiShop;
+use tiFy\Plugins\WooCommerce\Order;
+use tiFy\Plugins\WooCommerce\PaymentGateway\PaymentGateway;
+use tiFy\Plugins\WooCommerce\Query;
+use tiFy\Plugins\WooCommerce\ScriptLoader;
+use tiFy\Plugins\WooCommerce\Shipping;
+use tiFy\Plugins\WooCommerce\Shortcodes;
+use tiFy\Plugins\WooCommerce\Template;
+use tiFy\Plugins\WooCommerce\TemplateHooks;
+use tiFy\Plugins\WooCommerce\TemplateLoader;
 use tiFy\tiFy;
 
 class WooCommerce extends Plugin
 {
     /**
      * CONSTRUCTEUR
+     *
+     * @return void
      */
     public function __construct()
     {
         parent::__construct();
 
-        // Déclenchement des événements
-        $this->appAddAction('init', null, 1);
+        // Interface d'administration
+        $this->appServiceAdd(MetaBoxes::class, self::loadOverride(MetaBoxes::class));
+
+        // Panier
+        $this->appServiceAdd(Cart::class, self::loadOverride(Cart::class));
+
+        // Commande
+        $Checkout = self::getOverride(Checkout::class);
+        $this->appServiceAdd(Checkout::class, new $Checkout($this->appConfig('checkout')));
 
         // Chargement des controleurs
         // Identifiants de contexte
-        new ConditionalTags;
+        $this->appServiceAdd(ConditionalTags::class, new ConditionalTags());
 
         // Tests et modification des emails
-        $Forms = self::getOverride('\tiFy\Plugins\WooCommerce\Emails');
-        new Emails(self::tFyAppConfig('emails'));
+        $Emails = self::getOverride(Emails::class);
+        $this->appServiceAdd(Emails::class, new $Emails($this->appConfig('emails')));
 
         // Modification des formulaires
-        $Forms = self::getOverride( '\tiFy\Plugins\WooCommerce\Forms' );
-        new $Forms( self::tFyAppConfig( 'forms' ) );
-        
+        $Forms = self::getOverride(Forms::class);
+        $this->appServiceAdd(Forms::class, new $Forms($this->appConfig('forms')));
+
+        // Paiement
+        $this->appServiceAdd(Order::class, self::loadOverride(Order::class));
+
         // Requête de récupération des éléments de la boutique
-        self::loadOverride( '\tiFy\Plugins\WooCommerce\Query' );
-        
+        $this->appServiceAdd(Query::class, self::loadOverride(Query::class));
+
         // Chargement des scripts
         // Commande
-        $ScriptLoader = self::getOverride( '\tiFy\Plugins\WooCommerce\ScriptLoader' );
-        new $ScriptLoader( self::tFyAppConfig( 'script_loader' ) );
-        
+        $ScriptLoader = self::getOverride(ScriptLoader::class);
+        $this->appServiceAdd(ScriptLoader::class, new $ScriptLoader($this->appConfig('script_loader')));
+
+        // Livraison
+        $Shipping = self::getOverride(Shipping::class);
+        $this->appServiceAdd(Shipping::class, new $Shipping(self::tFyAppConfig('shipping')));
+
         // Gestionnaire de shortcodes
-        new Shortcodes( self::tFyAppConfig( 'shortcodes' ) );
-        
+        $Shortcodes = self::getOverride(Shortcodes::class);
+        $this->appServiceAdd(Shortcodes::class, new $Shortcodes($this->appConfig('shortcodes')));
+
         // Eléments de templates
-        self::loadOverride('\tiFy\Plugins\WooCommerce\Template');
-        
+        $this->appServiceAdd(Template::class, self::loadOverride(Template::class));
+
         // Surchage des fonctions de template Woocommerce
-        include self::tFyAppDirname() .'/TemplateFunctions.php';
+        include $this->appDirname() . '/TemplateFunctions.php';
         self::getOverrideAppFile('TemplateFunctions.php');
 
         // Accrochage / Décrochage / Ordonnacement des éléments de template
-        $templateHooks = self::getOverride('\tiFy\Plugins\WooCommerce\TemplateHooks');
-        new $templateHooks( self::tFyAppConfig( 'template-hooks' ) );
-        
-        // Panier
-        $Cart = self::loadOverride( '\tiFy\Plugins\WooCommerce\Cart' );
-        
-        // Livraison
-        $Shipping = self::getOverride( '\tiFy\Plugins\WooCommerce\Shipping' );
-        new $Shipping( self::tFyAppConfig('shipping'));
-        
-        // Commande
-        $Checkout = self::getOverride( '\tiFy\Plugins\WooCommerce\CheckOut' );
-        new $Checkout( self::tFyAppConfig('checkout') );
-        
-        // Paiement
-        self::loadOverride( '\tiFy\Plugins\WooCommerce\Order' );
-                
+        $TemplateHooks = self::getOverride(TemplateHooks::class);
+        $this->appServiceAdd(TemplateHooks::class, new $TemplateHooks($this->appConfig('template-hooks')));
+
         // Plateformes de paiement    
-        if( self::tFyAppConfig( 'payment_gateway' ) ) :
-            new PaymentGateway\PaymentGateway( self::tFyAppConfig( 'payment_gateway' ) );
+        if ($payment_gateway_conf = $this->appConfig('payment_gateway')) :
+            $PaymentGateway = self::getOverride(PaymentGateway::class);
+            $this->appServiceAdd(PaymentGateway::class ,new $PaymentGateway($payment_gateway_conf));
         endif;
-        
+
         // Boutique multiple
-        if( self::tFyAppConfig( 'multishop' ) ) :
-            new MultiShop\MultiShop( self::tFyAppConfig( 'multishop' ) );
+        if ($multishop_conf = $this->appConfig('multishop')) :
+            $MultiShop = self::getOverride(MultiShop::class);
+            $this->appServiceAdd(MultiShop::class, new $MultiShop($multishop_conf));
         endif;
-    
-        $appClassname = tiFy::getConfig('app.namespace') . "\\" . tiFy::getConfig('app.bootstrap');
+
         // Chargement des templates avec le moteur de gabarit PHP Plates
-        $service = self::tFyAppConfig('template_loader') ? : (class_exists($appClassname) ? $appClassname : '');
-        if($app = $this->appServiceGet($service)) :
+        $appClassname = tiFy::getConfig('app.namespace') . "\\" . tiFy::getConfig('app.bootstrap');
+        $service = $this->appConfig('template_loader') ?: (class_exists($appClassname) ? $appClassname : '');
+        if ($app = $this->appServiceGet($service)) :
             new TemplateLoader($app);
         endif;
 
         // Fonctions d'aide à la saisie
-        include self::tFyAppDirname() .'/Helpers.php';
-        
-        // Interface d'administration
-        self::loadOverride( '\tiFy\Plugins\WooCommerce\Admin\Product\MetaBoxes\MetaBoxes' );
+        include $this->appDirname() . '/Helpers.php';
+
+        // Déclenchement des événements
+        $this->appAddAction('init', null, 1);
     }
-    
+
     /**
-     * DECLENCHEURS
-     */
-    /**
-     * Initialisation globale de Wordpress
+     * Initialisation globale de Wordpress.
      *
      * @return void
      */
