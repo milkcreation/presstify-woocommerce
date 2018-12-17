f9349352d7fac69f6dc1b8f11985fea614e9ee4d
Jordy Manner <jordy.manner@milkcreation.fr>
2018-12-17 16:22:19 +0000

    woocommerce -- branch 2.0 -- finalisation réécriture compatibilité v2

--- Form/Form.php
+++ Form/Form.php
@@ -4,6 +4,8 @@ namespace tiFy\Plugins\Woocommerce\Form;
 
 use tiFy\Kernel\Params\ParamsBag;
 use tiFy\Plugins\Woocommerce\Contracts\Form as FormContract;
+use tiFy\Plugins\Woocommerce\WoocommerceResolverTrait;
+use WC;
 
 /**
  * FORMULAIRES
@@ -12,11 +14,7 @@ use tiFy\Plugins\Woocommerce\Contracts\Form as FormContract;
  */
 class Form extends ParamsBag implements FormContract
 {
-    /**
-     * Remplacement de la liste de sélection des pays par une liste de type tiFySelectJs.
-     * @var bool
-     */
-    protected $tiFySelectJsCountry = false;
+    use WoocommerceResolverTrait;
 
     /**
      * CONSTRUCTEUR.
@@ -27,7 +25,9 @@ class Form extends ParamsBag implements FormContract
     {
         parent::__construct($attrs);
 
-        $this->tiFySelectJsCountry = $this->get('tify_select_js_country', $this->tiFySelectJsCountry);
+        $this->selectJs();
+        $this->selectJsCountry();
+        $this->selectJsState();
 
         $this->addAddressFields($this->get('add_address_fields', []));
         $this->setAddressFormFields('billing', $this->get('billing', []));
@@ -36,9 +36,7 @@ class Form extends ParamsBag implements FormContract
 
         $this->setFormFieldArgs();
 
-
-        // Court-circuitage des attributs de champ de formulaire
-        //add_filter('woocommerce_form_field_tify_dropdown_country', [$this, 'woocommerce_form_field_tify_dropdown_country'], 10, 4);
+        $this->saveAddress();
     }
 
     /**
@@ -79,7 +77,15 @@ class Form extends ParamsBag implements FormContract
                                     $pos = array_search("{$addressType}_{$adminAttrs['before']}", array_keys($fields[$addressType]['fields']));
                                     $fields[$addressType]['fields'] = array_merge(
                                         array_slice($fields[$addressType]['fields'], 0, $pos),
-                                        [$_slug => $adminAttrs],
+                                        [$_slug => array_merge(
+                                            [
+                                                'label'       => isset($attrs['label']) ? $attrs['label'] : '',
+                                                'description' => '',
+                                                'class'       => ''
+                                            ],
+                                            $adminAttrs
+                                        )
+                                        ],
                                         array_slice($fields[$addressType]['fields'], $pos)
                                     );
                                 else :
@@ -96,6 +102,34 @@ class Form extends ParamsBag implements FormContract
     }
 
     /**
+     * Sauvegarde des adresses de facturation et livraison.
+     *
+     * @return void
+     */
+    protected function saveAddress()
+    {
+        add_action('woocommerce_after_save_address_validation',
+            function ($user_id, $load_address, $address) {
+                foreach ($address as $key => $field) :
+                    if (!isset($field['type'])) :
+                        $field['type'] = 'text';
+                    endif;
+                    switch ($field['type']) :
+                        case 'checkbox' :
+                            $_POST[$key] = (int)isset($_POST[$key]);
+                            break;
+                        default :
+                            $_POST[$key] = isset($_POST[$key]) ? wc_clean(wp_unslash($_POST[$key])) : '';
+                            break;
+                    endswitch;
+                endforeach;
+            },
+            10,
+            3
+        );
+    }
+
+    /**
      * Court-circuitage des attributs de champ de formulaire.
      *
      * @return void
@@ -105,10 +139,6 @@ class Form extends ParamsBag implements FormContract
         add_filter(
             'woocommerce_form_field_args',
             function ($args, $key, $value) {
-                if (in_array($key, ['billing_country', 'shipping_country']) && $this->tiFySelectJsCountry) :
-                    $args['type'] = 'tify_select_js_country';
-                endif;
-
                 if (method_exists($this, 'form_field_args_' . $key)) :
                     return call_user_func([$this, 'form_field_args_' . $key], $args, $value);
                 else :
@@ -187,68 +217,23 @@ class Form extends ParamsBag implements FormContract
     }
 
     /**
+     * Récupération des attributs HTML personnalisés d'un tag au format string.
      *
-     * @see wp-content/plugins/woocommerce/includes/wc-template-functions.php
+     * @param array $customAttrs Liste des attributs HTML personnalisés (clé => valeur)
+     *
+     * @return string
      */
-    /*final public function woocommerce_form_field_tify_dropdown_country($field, $key, $args, $value)
+    public function getCustomHtmlAttrs($customAttrs)
     {
-        $field = "";
+        $custom_attributes = [];
 
-        if ($args['required']) :
-            $args['class'][] = 'validate-required';
-            $required = ' <abbr class="required" title="' . esc_attr__('required', 'woocommerce') . '">*</abbr>';
-        else :
-            $required = '';
+        if (!empty($customAttrs) && is_array($customAttrs)) :
+            foreach ($customAttrs as $attribute => $attribute_value) :
+                $custom_attributes[] = esc_attr($attribute) . '="' . esc_attr($attribute_value) . '"';
+            endforeach;
         endif;
 
-        if ($args['label']) :
-            $field .= '<label for="' . esc_attr($args['id']) . '" class="' . esc_attr(implode(' ', $args['label_class'])) . '">' . $args['label'] . $required . '</label>';
-        endif;
-
-        $sort = $args['priority'] ? $args['priority'] : '';
-        $field_container = '<div class="form-row %1$s" id="%2$s" data-priority="' . esc_attr($sort) . '">%3$s</div>';
-        $countries = 'shipping_country' === $key ? WC()->countries->get_shipping_countries() : WC()->countries->get_allowed_countries();
-
-        if (1 === sizeof($countries)) :
-            $field .= '<strong>' . current(array_values($countries)) . '</strong>';
-            $field .= '<input type="hidden" name="' . esc_attr($key) . '" id="' . esc_attr($args['id']) . '" value="' . current(array_keys($countries)) . '" ' . implode(' ', array_filter((array)$args['custom_attributes'])) . ' class="country_to_state" />';
-        else :
-            $field .= tify_control_dropdown(
-                [
-                    'name'              => esc_attr($key),
-                    // 'id'                => esc_attr( $args['id'] ), // BUG avec le checkoutjs
-                    'class'             => 'country_to_state country_select ' . esc_attr(implode(' ', $args['input_class'])),
-                    'attrs'             => array_filter((array)$args['custom_attributes']),
-                    'show_option_none'  => esc_html__('Select a country&hellip;', 'woocommerce'),
-                    'option_none_value' => '',
-                    'choices'           => $countries,
-                    'selected'          => $value,
-                    'picker'            => [
-                        'class' => 'country_to_state country_select_picker ' . (isset($args['picker_class']) ? esc_attr(implode(' ', $args['picker_class'])) : null)
-                    ]
-                ],
-                false
-            );
-            $field .= '<noscript><input type="submit" name="woocommerce_checkout_update_totals" value="' . esc_attr__('Update country', 'woocommerce') . '" /></noscript>';
-        endif;
-
-        if ($args['description']) :
-            $field .= '<span class="description">' . esc_html($args['description']) . '</span>';
-        endif;
-
-        $container_class = esc_attr(implode(' ', $args['class']));
-        $container_id = esc_attr($args['id']) . '_field';
-        $field = sprintf($field_container, $container_class, $container_id, $field);
-
-        return $field;
-    }*/
-
-    /**
-     * {@inheritdoc}
-     */
-    public function istiFySelectJsCountry()
-    {
-        return $this->tiFySelectJsCountry;
+        return implode(' ', $custom_attributes);
     }
 
     /**
@@ -290,5 +275,167 @@ class Form extends ParamsBag implements FormContract
     public function form_field_args_billing_first_name($args, $value)
     {
         return $this->form_fields_args($args, 'billing_first_name', $value);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function hasSelectJsField($haystack)
+    {
+        foreach ($haystack as $slug => $attrs) :
+            if (is_array($attrs) && $this->hasSelectJsField($attrs)) :
+                return true;
+            elseif (($slug === 'type') && in_array($attrs, ['select_js', 'select_js_country'])) :
+                return true;
+            endif;
+        endforeach;
+
+        return false;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function isSelectJsEnabled()
+    {
+        foreach (['add_address_fields', 'billing', 'shipping', 'checkout'] as $form) :
+            if ($this->hasSelectJsField($this->get($form, []))) :
+                return true;
+            endif;
+        endforeach;
+
+        return false;
+    }
+
+    /**
+     * Définition du type de champ "Sélecteur JS".
+     *
+     * @see wp-content/plugins/woocommerce/includes/wc-template-functions.php
+     *
+     * @return void
+     */
+    protected function selectJs()
+    {
+        add_filter(
+            'woocommerce_form_field_select_js',
+            function ($field, $key, $args, $value) {
+                $field_type = 'select-js';
+
+                $args['attrs'] = array_merge(
+                    [
+                        'class' => esc_attr(implode(' ', $args['input_class']))
+                    ],
+                    isset($args['attrs']) ? $args['attrs'] : []
+                );
+
+                $args['picker'] = array_merge(
+                    [
+                        'class' => isset($args['picker_class']) ? esc_attr(implode(' ', $args['picker_class'])) : null
+                    ],
+                    isset($args['picker']) ? $args['picker'] : []
+                );
+
+                $args['choices'] = isset($args['options']) ? $args['options'] : [];
+
+                return (string)$this->viewer('field/field', compact('args', 'key', 'value', 'field_type'));
+            },
+            10,
+            4
+        );
+    }
+
+    /**
+     * Définition du type de champ "Sélecteur JS de pays".
+     *
+     * @see wp-content/plugins/woocommerce/includes/wc-template-functions.php
+     *
+     * @return void
+     */
+    protected function selectJsCountry()
+    {
+        add_filter(
+            'woocommerce_form_field_select_js_country',
+            function ($field, $key, $args, $value) {
+                $countries = 'shipping_country' === $key ? WC()->countries->get_shipping_countries() : WC()->countries->get_allowed_countries();
+
+                $singleCountry = (1 === sizeof($countries));
+
+                $field_type = $singleCountry ? 'single-country' : 'select-js-country';
+
+                if (!$singleCountry) :
+                    $args['attrs'] = array_merge(
+                        [
+                            'class' => 'country_to_state country_select ' . esc_attr(implode(' ', $args['input_class']))
+                        ],
+                        isset($args['attrs']) ? $args['attrs'] : []
+                    );
+
+                    $args['picker'] = array_merge(
+                        [
+                            'class' => 'country_to_state country_select_picker ' . (isset($args['picker_class']) ? esc_attr(implode(' ', $args['picker_class'])) : null)
+                        ],
+                        isset($args['picker']) ? $args['picker'] : []
+                    );
+                    $countries = ['' => esc_html__('Select a country&hellip;', 'woocommerce')] + $countries;
+                    $args['attrs'] = array_merge($args['attrs'], $args['custom_attributes']);
+                else :
+                    $args['custom_attributes'] = $this->getCustomHtmlAttrs($args['custom_attributes']);
+                endif;
+
+                $args['choices'] = $countries;
+
+                return (string)$this->viewer('field/field', compact('args', 'key', 'value', 'field_type'));
+            },
+            10,
+            4
+        );
+    }
+
+    /**
+     * Définition du type de champ "Sélecteur JS d'état/comté".
+     *
+     * @see wp-content/plugins/woocommerce/includes/wc-template-functions.php
+     *
+     * @return void
+     */
+    protected function selectJsState()
+    {
+        add_filter(
+            'woocommerce_form_field_select_js_state',
+            function ($field, $key, $args, $value) {
+                $for_country = isset($args['country']) ? $args['country'] : WC()->checkout->get_value('billing_state' === $key ? 'billing_country' : 'shipping_country');
+                $states = WC()->countries->get_states($for_country);
+
+                if (is_array($states) && empty($states)) :
+                    $field_type = 'empty-state';
+                    $args['custom_attributes'] = $this->getCustomHtmlAttrs($args['custom_attributes']);
+                    $args['inline_style'] = 'style="display:none;"';
+                elseif (!is_null($for_country) && is_array($states)) :
+                    $field_type = 'select-js';
+                    $args['attrs'] = array_merge(
+                        [
+                            'class' => 'state_select ' . esc_attr(implode(' ', $args['input_class']))
+                        ],
+                        isset($args['attrs']) ? $args['attrs'] : []
+                    );
+
+                    $args['picker'] = array_merge(
+                        [
+                            'class' => 'state_select_picker ' . (isset($args['picker_class']) ? esc_attr(implode(' ', $args['picker_class'])) : null)
+                        ],
+                        isset($args['picker']) ? $args['picker'] : []
+                    );
+                    $args['choices'] = ['' => esc_html__('Select a state&hellip;', 'woocommerce')] + $states;
+                    $args['attrs'] = array_merge($args['attrs'], $args['custom_attributes']);
+                else :
+                    $field_type = 'text-state';
+                    $args['custom_attributes'] = $this->getCustomHtmlAttrs($args['custom_attributes']);
+                endif;
+
+                return (string)$this->viewer('field/field', compact('args', 'key', 'value', 'field_type'));
+            },
+            10,
+            4
+        );
     }
 }
