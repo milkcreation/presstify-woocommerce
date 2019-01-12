574d4e7b1133322238150d804a4ccaeeb12529a5
Jordy Manner <jordy.manner@milkcreation.fr>
2019-01-12 14:41:09 +0000

    woocommerce -- branch 2.0 -- Réécriture doc

--- Woocommerce.php
+++ Woocommerce.php
@@ -1,30 +1,25 @@
 <?php
 
-/**
- * @name Woocommerce
- * @desc Support de Woocommerce
- * @author Jordy Manner <jordy@milkcreation.fr> && Julien Picard <julien@tigreblanc.fr>
- * @package presstify-plugins/woocommerce
- * @namespace \tiFy\Plugins\Woocommerce
- * @version 2.0.0
- */
-
-/**
- * @see https://docs.woocommerce.com/wc-apidocs/index.html
- */
-
 namespace tiFy\Plugins\Woocommerce;
 
 use tiFy\Plugins\Woocommerce\Contracts\Woocommerce as WoocommerceContract;
 
 /**
  * Class Woocommerce
- * @package tiFy\Plugins\Woocommerce
  *
- * Activation :
- * ----------------------------------------------------------------------------------------------------
+ * @desc Extension PresstiFy de court-circuitage et de fonctionnalités complémentaires woocommerce.
+ * @author Jordy Manner <jordy@milkcreation.fr>
+ * @author Julien Picard <julien@tigreblanc.fr>
+ * @package tiFy\Plugins\Woocommerce
+ * @version 2.0.1
+ *
+ * @see https://docs.woocommerce.com/wc-apidocs/index.html
+ *
+ * USAGE :
+ * Activation
+ * ---------------------------------------------------------------------------------------------------------------------
  * Dans config/app.php ajouter \tiFy\Plugins\Woocommerce\WoocommerceServiceProvider à la liste des fournisseurs de
- * services chargés automatiquement par l'application.
+ * services.
  * ex.
  * <?php
  * ...
@@ -40,10 +35,10 @@ use tiFy\Plugins\Woocommerce\Contracts\Woocommerce as WoocommerceContract;
  *      ]
  * ];
  *
- * Configuration :
- * ----------------------------------------------------------------------------------------------------
+ * Configuration
+ * ---------------------------------------------------------------------------------------------------------------------
  * Dans le dossier de config, créer le fichier woocommerce.php
- * @see /vendor/presstify-plugins/woocommerce/Resources/config/woocommerce.php Exemple de configuration
+ * @see /vendor/presstify-plugins/woocommerce/Resources/config/woocommerce.php
  */
 class Woocommerce implements WoocommerceContract
 {
