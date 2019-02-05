53267347369549df164e123a4b49dc9c117f7da3
mastermilk <mastermilk@9c4951e9-fce0-42f0-91e9-f5562070ff10>
2019-02-05 16:46:48 +0000

    woocommerce -- tag 2.0.2 -- Réécriture passe #2

---
+++ woocommerce/tags/2.0.2/Checkout/Checkout.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace tiFy\Plugins\Woocommerce\Checkout;
+
+use tiFy\Kernel\Params\ParamsBag;
+use tiFy\Plugins\Woocommerce\Contracts\Checkout as CheckoutContract;
+
+class Checkout extends ParamsBag implements CheckoutContract
+{
+    /**
+     * Minimum de commande.
+     * @var array
+     */
+    protected $minPurchase = [
+        'rate'     => 0,
+        'based_on' => 'subtotal',
+        'notice'   => 'Désolé, le montant minimum des commandes est fixé à %s'
+    ];
+
+    /**
+     * CONSTRUCTEUR.
+     *
+     * @return void
+     */
+    public function __construct($attrs = [])
+    {
+        parent::__construct($attrs);
+
+        $this->setMinPurchase($this->get('min_purchase', []));
+        $this->bindToProcess($this->minPurchase);
+    }
+
+    /**
+     * Définition d'un montant minimum de commande.
+     *
+     * @param array $minPurchase Montant minimum de commande.
+     *
+     * @return void
+     */
+    protected function setMinPurchase($minPurchase)
+    {
+        if ($minPurchase) :
+            $this->minPurchase = array_merge($this->minPurchase, $minPurchase);
+            if (!in_array($this->minPurchase['based_on'], ['subtotal', 'subtotal_ex_tax', 'total'])) :
+                $this->minPurchase['based_on'] = 'subtotal';
+            endif;
+        endif;
+    }
+
+    /**
+     * Branchement du montant minimum de commande à l'environnement WooCommerce.
+     *
+     * @param array $minPurchase Montant minimum de commande.
+     *
+     * @return void
+     */
+    protected function bindToProcess($minPurchase)
+    {
+        if ($minPurchase['rate']) :
+            add_action(
+                'woocommerce_before_checkout_process',
+                function () use ($minPurchase) {
+                    $cartBase = WC()->cart->{$minPurchase['based_on']};
+                    if ($cartBase < $minPurchase['rate']) :
+                        wc_add_notice(
+                            sprintf($minPurchase['notice'], $minPurchase['rate'] . get_woocommerce_currency_symbol()),
+                            'error'
+                        );
+                    endif;
+                }
+            );
+        endif;
+    }
+}
