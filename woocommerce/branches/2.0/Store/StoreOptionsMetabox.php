40566538f9d2e0a802acea3563e4cc8609c16bb4
mastermilk <mastermilk@9c4951e9-fce0-42f0-91e9-f5562070ff10>
2019-10-08 13:28:03 +0000

    woocommerce -- branch 2.0 -- Modification Réécriture métabox

--- woocommerce/branches/2.0/Store/StoreOptionsMetabox.php
+++ woocommerce/branches/2.0/Store/StoreOptionsMetabox.php
@@ -24,42 +24,4 @@ class StoreOptionsMetabox extends AbstractStoreMetabox
     {
         return (string)$this->woocommerce()->viewer('store/options-general', $this->all());
     }
-
-    /**
-     * @param $value
-     *
-     * @return mixed
-     */
-    public function sanitize_tify_wc_page_display($value)
-    {
-        // Récupération de la catégorie affichée par la boutique
-        $cat = (int)get_option('tify_wc_'. $this->store()->getName() .'_cat', 0);
-
-        // Récupération de la catégorie liée à l'affichage de la page
-        if ($page_display_cat = (int)get_option('tify_wc_'. $this->store()->getName() .'_page_display_cat', 0) ) {
-            if ($cat !== $page_display_cat) {
-                delete_term_meta($page_display_cat, 'display_type');
-            }
-        }
-
-        update_option('tify_wc_'. $this->store()->getName() .'_page_display_cat', $cat);
-        update_term_meta($cat, 'display_type', $value);
-
-        return $value;
-    }
-
-    /**
-     * @inheritDoc
-     */
-    public function settings()
-    {
-        return [
-            'tify_wc_'. $this->store()->getName() .'_hook',
-            'tify_wc_'. $this->store()->getName() .'_cat',
-            'tify_wc_'. $this->store()->getName() .'_page_display' => [
-                'sanitize_callback' => [$this, 'sanitize_tify_wc_page_display']
-            ],
-            'tify_wc_multi_default'
-        ];
-    }
 }
