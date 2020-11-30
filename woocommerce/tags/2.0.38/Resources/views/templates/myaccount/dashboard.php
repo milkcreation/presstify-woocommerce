c0236414b18589f685b25f47bb68d9cd578f227f
5upp0r7 <5upp0r7@9c4951e9-fce0-42f0-91e9-f5562070ff10>
2020-11-30 19:01:48 +0000

    woocommerce v2.0.38

---
+++ woocommerce/tags/2.0.38/Resources/views/templates/myaccount/dashboard.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Woocommerce - Mon Compte - Tableau de bord
+ * ---------------------------------------------------------------------------------------------------------------------
+ * @version     2.6.0
+ *
+ * @var App\Views\ViewController $this
+ */
+?>
+
+<?php $this->layout('wc.myaccount::my-account'); ?>
+
+<section class="MyAccountDashboard">
+    <p class="MyAccountDashboard-title">
+        <?php
+        printf(
+            __('Bonjour <strong>%1$s</strong> <br>(Vous n\'êtes pas %1$s? <a href="%2$s">Déconnexion</a>)', 'tify'),
+            esc_html($current_user->display_name),
+            esc_url(wc_logout_url(wc_get_page_permalink('myaccount')))
+        );
+        ?>
+    </p>
+
+    <div class="Dashboard-items">
+        <ul class="row">
+            <li class="col-12 col-sm-6 col-lg-4 col-xl-3">
+                <div class="Dashboard-itemInner">
+                    <?php _e('Consulter mes commandes', 'tify'); ?>
+                    <a href="<?php echo esc_url(wc_get_endpoint_url('orders')); ?>" class="Dashboard-itemLink"
+                       title="<?php _e('Consulter mes commandes', 'tify') ?>"></a>
+                </div>
+            </li>
+
+            <li class="col-12 col-sm-6 col-lg-4 col-xl-3">
+                <div class="Dashboard-itemInner">
+                    <?php _e('Consulter mes adresses', 'tify'); ?>
+                    <a href="<?php echo esc_url(wc_get_endpoint_url('edit-address')); ?>" class="Dashboard-itemLink"
+                       title="<?php _e('Consulter mes adresses', 'tify'); ?>"></a>
+                </div>
+            </li>
+
+            <li class="col-12 col-sm-6 col-lg-4 col-xl-3">
+                <div class="Dashboard-itemInner">
+                    <?php _e('Consulter le détail de mon compte', 'tify'); ?>
+                    <a href="<?php echo esc_url(wc_get_endpoint_url('edit-account')); ?>" class="Dashboard-itemLink"
+                       title="<?php _e('Consulter le détail de mon compte', 'tify'); ?>"></a>
+                </div>
+            </li>
+        </ul>
+    </div>
+
+    <?php echo $this->accountReadmore(); ?>
+</section>
